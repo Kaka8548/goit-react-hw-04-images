239bdfd312c3c5d40a00aa9987cc372752bde13e
Kaka8548 <katyakatya8548@gmail.com>
2023-01-14 21:24:38 +0400

    Task 3: App component was refactored using React Hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,66 +2,60 @@ import { Application } from './App.styled';
 import Button from './button/Button';
 import ImageGallery from './imageGallery/ImageGallery';
 import Searchbar from './searchbar/Searchbar';
-import { Component } from 'react';
 import { filterHIts, getImages } from 'utils';
 import Loader from './loader/Loader';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
-export class App extends Component {
-  state = {
-    query: '',
-    page: 1,
-    images: '',
-    isLoading: false,
-    totalImages: 0,
-    error: '',
-  };
+export function App() {
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [images, setImages] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [totalImages, setTotalImages] = useState(0);
+  const [error, setError] = useState('');
 
-  componentDidUpdate = async (_, prevState) => {
-    const { query, page } = this.state;
-    if (prevState.query !== query || prevState.page !== page) {
-      this.setState({ isLoading: true });
+  useEffect(() => {
+    if (!query) return;
+
+    setIsLoading(true);
+    const fetchData = async () => {
       try {
         const data = await getImages({ query, page });
         const filteredHits = filterHIts(data.hits);
 
-        this.setState(prevState => ({
-          images: [...prevState.images, ...filteredHits],
-          totalImages: data.totalHits,
-          error: '',
-        }));
+        setImages(prevState => [...prevState, ...filteredHits]);
+        setTotalImages(data.totalHits);
+        setError('');
       } catch (error) {
-        this.setState({ error: error });
+        setError({ error });
       } finally {
-        this.setState({ isLoading: false });
+        setIsLoading(false);
       }
-    }
+    };
+    fetchData();
+  }, [query, page]);
+
+  const handleSubmit = normilizedQuery => {
+    setQuery(normilizedQuery);
+    setImages([]);
+    setPage(1);
+    setTotalImages(0);
   };
 
-  handleSubmit = normilizedQuery => {
-    this.setState({
-      query: normilizedQuery,
-      images: [],
-      page: 1,
-      totalImages: 0,
-    });
+  const handleClick = () => {
+    setPage(prevState => prevState + 1);
   };
 
-  handleClick = () => {
-    this.setState(prevState => ({ page: prevState.page + 1 }));
-  };
+  const isBtnShown = totalImages !== images.length && !isLoading;
 
-  render() {
-    const { images, isLoading, totalImages, error } = this.state;
-    const isBtnShown = totalImages !== images.length && !isLoading;
-
-    return (
-      <Application>
-        <Searchbar handleSubmit={this.handleSubmit} />
-        {images.length > 0 && <ImageGallery images={images} />}
-        {isBtnShown && <Button onClick={this.handleClick} />}
-        {isLoading && <Loader />}
-        {error && <p>{error}</p>}
-      </Application>
-    );
-  }
+  return (
+    <Application>
+      <Searchbar handleSubmit={handleSubmit} />
+      {images.length > 0 && <ImageGallery images={images} />}
+      {isBtnShown && <Button onClick={handleClick} />}
+      {isLoading && <Loader />}
+      {error && <p>{error}</p>}
+    </Application>
+  );
 }
