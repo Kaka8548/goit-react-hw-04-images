cb6653e2175bfe62d0ff4b4c401e7c2935b89e50
Kaka8548 <katyakatya8548@gmail.com>
2023-01-14 20:45:53 +0400

    Task 3: Modal component was refactored using React Hooks

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,8 +1,37 @@
 import { createPortal } from 'react-dom';
 import { ModalWindow, Overlay } from './Modal.styled';
 import PropTypes from 'prop-types';
+import { useEffect } from 'react';
 
 const modalElement = document.querySelector('#modal-root');
+
+export default function Modal({ bigUrl, alt, onClick }) {
+  useEffect(() => {
+    const closeModal = () => {
+      onClick();
+    };
+
+    document.addEventListener('keydown', closeModal);
+
+    return document.removeEventListener('keydown', closeModal);
+  }, [onClick]);
+
+  const handleClick = event => {
+    if (event.target === event.currentTarget) {
+      onClick();
+    }
+  };
+
+  return createPortal(
+    <Overlay onClick={handleClick}>
+      <ModalWindow className="Modal">
+        <img src={bigUrl} alt={alt} />
+      </ModalWindow>
+    </Overlay>,
+    modalElement
+  );
+}
+
 Modal.propTypes = {
   onClick: PropTypes.func.isRequired,
   bigUrl: PropTypes.string.isRequired,
